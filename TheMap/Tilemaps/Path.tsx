35011584434690de4717f1a09d39a737106c71a5
Chardonneaur <contact@ronan-chardonneau.fr>
2023-10-14 21:14:51 +0200

    Add files via upload

---
+++ TheMap/Tilemaps/Path.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Path" tilewidth="32" tileheight="32" tilecount="48" columns="12">
+ <image source="../../Path.png" width="384" height="128"/>
+</tileset>
