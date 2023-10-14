84d690d45768c2290bb024bd037a932224e1f55e
Chardonneaur <contact@ronan-chardonneau.fr>
2023-10-14 21:13:13 +0200

    Add files via upload

---
+++ TheMap/WA_Seats.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="WA_Seats" tilewidth="32" tileheight="32" tilecount="182" columns="13">
+ <image source="../map-starter-kit-master/map-starter-kit-master/tilesets/WA_Seats.png" width="416" height="448"/>
+</tileset>
